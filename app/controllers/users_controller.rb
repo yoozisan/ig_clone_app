158d2e260fd288031f28cd2209068668a3f3586e
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-12 14:08:13 +0900

    サインアップ機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
