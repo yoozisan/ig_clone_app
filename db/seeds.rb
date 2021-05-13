97fff7a86235c5d10f177ae791fcc6aa46916339
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-14 00:29:33 +0900

    seedデータ作成、404,500エラー実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  name = Faker::Name.first_name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
