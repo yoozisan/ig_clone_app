621c7c990dec747435959dcd481d48062e26a657
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-14 00:20:57 +0900

    letter_opener_web設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'favorites/index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :favorites, only: [:create, :destroy, :show]
+  root to: 'sessions#new'
   resources :pictures do
     collection do
       post :confirm
 		end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
